1c6f6d7b76b21f159fe6910d02111dfb71c0bdf4
Nikeshkamble25 <Nikeshkamble25@gmail.com>
2021-06-07 21:22:53 +0530

    Added Prototype pattern

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Id = 1;
+            employee.Name = "Nikesh Kamble";
+            employee.DOB = DateTime.Now.AddYears(-30);
+
+            IClonable<Employee> empCloningObject = employee;
+            Employee employeeCloned = empCloningObject.Clone();
+            
+            Console.ReadLine();
+        }
+    }
+    public interface IClonable<T>
+    {
+        T Clone();
+    }
+    public class Employee : IClonable<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public Employee Clone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+}
