8ba44bc57350f3ec1846a0446013435942edb4cb
Nikeshkamble25 <Nikeshkamble25@gmail.com>
2021-06-12 21:38:43 +0530

    Chain Of Resposnibility pattern Added

---
+++ ChainofResposnibility/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ChainofResposnibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TradeFlow frontOffice = new FrontOffice();
+            TradeFlow middleOffice = new MiddleOffice();
+            TradeFlow backOffice = new BackOffice();
+            frontOffice.SetSuccessor(middleOffice);
+            middleOffice.SetSuccessor(backOffice);
+
+            frontOffice.ProcessTrade();
+
+            Console.ReadLine();
+        }
+    }
+    public abstract class TradeFlow
+    {
+        protected TradeFlow _tradeFlow = null;
+        public void SetSuccessor(TradeFlow tradeFlow)
+        {
+            _tradeFlow = tradeFlow;
+        }
+        public abstract void ProcessTrade();
+    }
+    public class BackOffice : TradeFlow
+    {
+        public override void ProcessTrade()
+        {
+            Console.WriteLine("Back Office Processed");
+            if (_tradeFlow != null)
+                _tradeFlow.ProcessTrade();
+        }
+    }
+    public class FrontOffice : TradeFlow
+    {
+        public override void ProcessTrade()
+        {
+            Console.WriteLine("Front Office Processed");
+            if (_tradeFlow != null)
+                _tradeFlow.ProcessTrade();
+        }
+    }
+    public class MiddleOffice : TradeFlow
+    {
+        public override void ProcessTrade()
+        {
+            Console.WriteLine("Middle Office Processed");
+            if (_tradeFlow != null)
+                _tradeFlow.ProcessTrade();
+        }
+    }
+}
